4a494c7f0fcb89e91712bd140acaa7ca0ef17115
FrancoRaymond <155026752+FrancoRaymond@users.noreply.github.com>
2025-05-13 18:15:45 +0200

    added hero markup

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,9 +4,16 @@ import "./hero.scss"
 const Hero = () => {
   return (
     <div className='hero'>
-      
+      <div className='heroTextContainer'>
+        <h1>A modern publishing platform</h1>
+        <p>Grow your audience and build your online brand.</p>
+        <div className='heroBtnDiv'>
+          <button>Start for free</button>
+          <button>Learn more</button>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Hero
+export default Hero;
