f0cfad70c5d40b24787376225ddd66e24f7694c2
FrancoRaymond <155026752+FrancoRaymond@users.noreply.github.com>
2025-05-31 21:09:50 +0200

    added free and open component styles

--- src/components/freeandopen/FreeAndOpen.jsx
+++ src/components/freeandopen/FreeAndOpen.jsx
@@ -5,14 +5,16 @@ import laptopMobile from '../../assets/images/illustration-laptop-mobile.svg'
 
 const FreeAndOpen = () => {
   return (
-    <div>
+    <div className='freeAndOpen'>
+      <div className='imageDiv'>
         <img src={laptopMobile} alt="" />
-        <div>
-            <h2>Free, open, simple</h2>
-            <p>Blogr iss a free and open source application backed by a large community of helpful developers. It supports features such as code syntax highlighting, RSS feeds, social media intergration, third-party commenting tools, and works seamlesly with google analytics. The architecture is clean and is relatively easy to learn.</p>
-            <h2>Poweful tooling</h2>
-            <p>Batteries including. We built a simple and straightfoward CLI tool that makes customization and deployment a breeze, but capable of producing even the most complicated sites.</p>
-        </div>
+      </div>
+      <div className='textDiv'>
+        <h2>Free, open, simple</h2>
+        <p>Blogr iss a free and open source application backed by a large community of helpful developers. It supports features such as code syntax highlighting, RSS feeds, social media intergration, third-party commenting tools, and works seamlesly with google analytics. The architecture is clean and is relatively easy to learn.</p>
+        <h2>Poweful tooling</h2>
+        <p>Batteries including. We built a simple and straightfoward CLI tool that makes customization and deployment a breeze, but capable of producing even the most complicated sites.</p>
+      </div>
     </div>
   )
 }
