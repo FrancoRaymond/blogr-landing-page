0f901d8337c809e70aa9b1a9542ff40fdece5108
FrancoRaymond <155026752+FrancoRaymond@users.noreply.github.com>
2025-06-03 18:04:58 +0200

    added component styles

--- src/components/stateofart/StateOfArt.jsx
+++ src/components/stateofart/StateOfArt.jsx
@@ -7,14 +7,16 @@ import circles from '../../assets/images/bg-pattern-circles.svg'
 const StateOfArt = () => {
   return (
     <div className='stateOfArt'>
-        <div className='imageDiv'>
-            <img src={phones} alt="" className='phones'/>
-            <img src={circles} alt="" className='circles'/>
-        </div>
+      <div className='imageDiv'>
+          <img src={phones} alt="" className='phones'/>
+          <img src={circles} alt="" className='circles'/>
+      </div>
+      <div className='textDiv'>
         <h2>State of the Art Infrastructure</h2>
-        <p>With reliability and speen in mind, worldwide data centers provide the backbone for ultra-fast connectivity. Thi8s ensures your site will load instantly, no matter where your raders are, keeping your site competitive.</p>
+        <p>With reliability and speen in mind, worldwide data centers provide the backbone for ultra-fast connectivity. Thi8s ensures your site will load instantly, no matter where your raders are, keeping your site competitive.</p>  
+      </div>
     </div>
   )
 }
 
-export default StateOfArt
+export default StateOfArt;
